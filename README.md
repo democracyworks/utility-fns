02678983b920a2ed1e5b473b9976032c905984aa
Chris Shea <cmshea@gmail.com>
2016-09-15 12:57:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# utility-works
+
+A Clojure library of utility functions.
+
+## License
+
+Copyright © 2016 Democracy Works, Inc.
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject democracyworks/utility-works "0.1.0-SNAPSHOT"
+  :description "Utility functions"
+  :url "https://github.com/democracyworks/utility-works"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])

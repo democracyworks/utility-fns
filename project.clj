4160125d52fcd2496cce5c60a152e3ad2dbf3e56
Christopher Shea <cmshea@gmail.com>
2016-10-06 19:13:36 -0400

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/utility-works "0.1.2"
+(defproject democracyworks/utility-works "0.1.3-SNAPSHOT"
   :description "Utility functions"
   :url "https://github.com/democracyworks/utility-works"
   :license {:name "Eclipse Public License"
